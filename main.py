7a821f865cd03f0ac701af8fe726d7baa61810c1
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-04-27 21:19:33 +0900

    add google oauth2.0 code

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from authlib.integrations.starlette_client import OAuth
+from starlette.requests import Request
+from starlette.config import Config
+from starlette.middleware.sessions import SessionMiddleware
 
 app = FastAPI()
+config = Config('.env')
+oauth = OAuth(config)
+app.add_middleware(SessionMiddleware, secret_key="!secret")
+
+CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+oauth.register(
+    name='google',
+    server_metadata_url=CONF_URL,
+    client_kwargs={
+        'scope': 'openid email profile',
+        'prompt': 'select_account',  # force to select account
+    }
+)
 
 # Add CORS middleware
 origins = [
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://accounts.google.com",
 ]
 
 app.add_middleware(
@@ -20,3 +38,16 @@ app.add_middleware(
 @app.get("/")
 def read_root():
     return {"message": "hello"}
+
+
+@app.get("/login")
+async def login_via_google(request: Request):
+    redirect_uri = 'http://localhost:3000/api/auth/callback/google'
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+
+@app.get("/api/auth/callback/google")
+async def auth_via_google(request: Request):
+    redirect_uri = 'http://localhost:3000/api/auth/callback/google'
+    token = await oauth.google.authorize_access_token(request, redirect_uri=redirect_uri)
+    user = await oauth.google.parse_id_token(request, token)
+    return dict(user)
